0c456c8b77c4f69f377151c8a2b7e3cd95e72798
TinySkyReally <tinyskyreally@gmail.com>
2025-01-19 01:04:11 +0100

    Update controlz.lua

--- controlz.lua
+++ controlz.lua
@@ -10,9 +10,9 @@
 ╚═════╝    ╚═╝       ╚══════╝╚═╝╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═══╝╚═╝  ╚═══╝   ╚═╝╚══════╝╚═╝      ╚═════╝ ╚═╝  ╚═╝     ╚═╝
 ]]
 
-getgenv().Username = "dontsleepallnight1" -- The username of the person you want to have full control over the bots.
+getgenv().Username = "dontsleepallnight" -- The username of the person you want to have full control over the bots.
 getgenv().Prefix = "." -- With what symbol the cmds should start with. (e.g. if its . then .cmds if its ; then ;cmds)
-getgenv().Bots = {"TinyControllBot"} -- The display names of the bots you gonna run the script on.
+getgenv().Bots = {"dontsleepallnight1", "dontsleepallnight2", "dontsleepallnight3", "dontsleepallnight4", "dontsleepallnight5"} -- The display names of the bots you gonna run the script on.
 getgenv().logChat = false -- If true it will start logging message to the discord webhook after running the script. If false it wont start logging messages until you run logchat enable.
 getgenv().webhook = "https://discord.com/api/webhooks/1330323068090060860/HWqU4z4-yBPQoomH3lHVRUPSPAYz-5HQrP3qa9SiIotAxhQLYDs3cTn3hNA7S17jXx9p"  -- Discord webhook url for logging.
 
